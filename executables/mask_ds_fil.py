Fixed major RFI masking issues.

executables/mask_ds_fil.py
@@ -18,6 +18,7 @@ from modules.read_header import Header
 from modules.read_fil import load_fil_data
 from modules.read_rfifindmask import read_rfimask, modify_zapchans_bandpass
 from modules.ds_systematics import remove_additive_time_noise, calc_median_bandpass, correct_bandpass
+from modules.module_dedisp import calc_tDM, dedisperse_ds
 from modules.filters1d import blockavg1d
 from modules.filters2d import smooth_master
 from modules.general_utils import setup_logger_stdout, create_dir
@@ -32,7 +33,7 @@ from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 #########################################################################
 # Execute call  for child processors.
-def myexecute(hotpotato, times_chunk, tstart, tstop, freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_chunk, int_times_chunk, mask_zap_chans_per_intchunk, logger, rank):
+def myexecute(hotpotato, times_chunk, tstart, tstop, freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_chunk, int_times_chunk, mask_zap_chans_per_intchunk, mask_zap_chans, logger, rank):
     # Read in a chunk of filterbank data.
     logger.info('RANK %d: Reading in data between times %.2f - %.2f s'% (rank, times_chunk[0], times_chunk[-1]))
     f = open(hotpotato['DATA_DIR']+'/'+hotpotato['fil_file'],'rb')
@@ -58,6 +59,7 @@ def myexecute(hotpotato, times_chunk, tstart, tstop, freqs_GHz, npol, nchans, ch
     if hotpotato['remove_zerodm']:
         data = remove_additive_time_noise(data)[0]
         logger.info('RANK %d: Zerodm removal completed.'% (rank))
+    data = np.zeros((len(freqs_GHz), len(times_chunk)))
     # Apply rfifind mask on data.
     boolean_rfimask = np.zeros(data.shape,dtype=bool)
     for i in range(nint_chunk):
@@ -72,28 +74,67 @@ def myexecute(hotpotato, times_chunk, tstart, tstop, freqs_GHz, npol, nchans, ch
     # Replaced masked entries with mean value.
     logger.info('RANK %d: Replacing masked entries with mean values'% (rank))
     data = np.ma.filled(data, fill_value=np.nanmean(data))
+    # Dedisperse the data, if DM is non-zero.
+    if hotpotato['DM']!=0:
+        logger.info('RANK %d: Dedispersing data using DM = %.1f pc/cc'% (rank, hotpotato['DM']))
+        data, times_chunk = dedisperse_ds(data, freqs_GHz, hotpotato['DM'], freqs_GHz[-1], freqs_GHz[0], times_chunk[1]-times_chunk[0], times_chunk[0])
     # Smooth and downsample the data.
     data, freqs_GHz, times_chunk = smooth_master(data,hotpotato['smoothing_method'],hotpotato['convolution_method'],hotpotato['kernel_size_freq_chans'],hotpotato['kernel_size_time_samples'],freqs_GHz,times_chunk)
     if hotpotato['smoothing_method']!='Blockavg2D':
         data, freqs_GHz, times_chunk = smooth_master(data,'Blockavg2D',hotpotato['convolution_method'],hotpotato['kernel_size_freq_chans'],hotpotato['kernel_size_time_samples'],freqs_GHz,times_chunk)
+    mask_zap_check = list(np.sort(mask_zap_chans)//hotpotato['kernel_size_freq_chans'])
+    mask_chans = np.array([chan for chan in np.unique(mask_zap_check) if mask_zap_check.count(chan)==hotpotato['kernel_size_freq_chans']])
     logger.info('RANK %d: Data smoothing and/or downsampling completed.'% (rank))
     # Remove residual spectral trend.
     data = data - np.median(data,axis=1)[:,None]
     logger.info('RANK %d: Residual spectral trend subtracted.'% (rank))
     # Write a .npz file to disk.
-    save_array = [data, freqs_GHz, times_chunk]
-    save_keywords = ['DS', 'Freqs (GHz)', 'Time (s)']
+    save_array = [data, freqs_GHz, times_chunk, mask_chans]
+    save_keywords = ['DS', 'Radio frequency (GHz)', 'Time (s)', 'Channel mask']
     np.savez(hotpotato['OUTPUT_DIR']+'/'+hotpotato['basename']+'_dsamp_tstart%08.2f'% (times_chunk[0]), **{name:value for name, value in zip(save_keywords,save_array)})
 
+# Collate data from multiple small .npz files into a single large .npz file.
+def collate_npz_to_npz(hdr, hotpotato, logger, remove_npz=True):
+    logger.info('Collating data from multiple temporary .npz files into a single .npz  file')
+    npz_list = sorted(glob.glob(hotpotato['OUTPUT_DIR']+'/'+hotpotato['basename']+'_dsamp_tstart*.npz'))
+    if len(npz_list)==0:
+        logger.warning('No .npz files detected. Quitting...')
+        sys.exit(1)
+    contents = np.load(npz_list[0], allow_pickle=True)
+    freqs_GHz = contents['Radio frequency (GHz)']
+    data = contents['DS']
+    time_array = contents['Time (s)']
+    mask_chans = contents['Channel mask']
+    if len(npz_list)>1:
+        for i in range(1,len(npz_list)):
+            npz_append_contents = np.load(npz_list[i], allow_pickle=True)
+            data = np.concatenate((data, npz_append_contents['DS']), axis=1)
+            time_array = np.concatenate((time_array, npz_append_contents['Time (s)']))
+    save_array = [data, freqs_GHz, time_array, mask_chans]
+    save_keywords = ['DS', 'Radio frequency (GHz)', 'Time (s)', 'Channel mask']
+    out_npz_filename = hotpotato['OUTPUT_DIR']+'/'+hotpotato['basename']+'_dsamp'
+    logger.info('Writing data into .npz file %s'% (out_npz_filename))
+    np.savez(out_npz_filename, **{name:value for name, value in zip(save_keywords,save_array)})
+    logger.info('Write complete.')
+    # Remove temporary npz files.
+    if remove_npz:
+        for npz_file in npz_list:
+            rm_cmd = 'rm %s'% (npz_file)
+            status = sp.check_call(rm_cmd, shell=True)
+            if status==0:
+                logger.info('Deleted %s.'% (npz_file))
+            else:
+                logger.debug('Error deleting %s.'% (npz_file))
+
 # Collate data from temporary .npz files into a filterbank data product.
-def collate_npz(hdr, hotpotato, logger, remove_npz=True):
+def collate_npz_to_fil(hdr, hotpotato, logger, remove_npz=True):
     logger.info('Collating data from multiple temporary .npz files into a single filterbank file')
     npz_list = sorted(glob.glob(hotpotato['OUTPUT_DIR']+'/'+hotpotato['basename']+'_dsamp_tstart*.npz'))
     if len(npz_list)==0:
         logger.warning('No .npz files detected. Quitting...')
         sys.exit(1)
     contents = np.load(npz_list[0], allow_pickle=True)
-    freqs_GHz = contents['Freqs (GHz)']
+    freqs_GHz = contents['Radio frequency (GHz)']
     data = contents['DS']
     time_array = contents['Time (s)']
     if len(npz_list)>1:
@@ -130,26 +171,28 @@ def collate_npz(hdr, hotpotato, logger, remove_npz=True):
                 logger.debug('Error deleting %s.'% (npz_file))
 
 # Divide time axis into multiple chunks, one chunk per child processor. Each chunk is further subdivided into blocks.
-def divide_timechunks(nproc, nblocks, times, nint, int_times, mask_zap_chans_per_int, t_samp, downsample_time_factor):
+def divide_timechunks(nproc, nblocks, times, nint, int_times, mask_zap_chans_per_int, t_samp, downsample_time_factor, freq_low, DM, ref_freq):
+    DMcurv_samples = np.round(calc_tDM(freq_low,DM,ref_freq)/t_samp).astype(int) # No. of samples spanning a broadband DM curve
+    print('No. of samples spanning a broadband DM curve from %.2f GHz to %.2f GHz = %d'% (ref_freq, freq_low, DMcurv_samples))
     if nproc>1:
-        equal_divide_size = np.round(len(times)/(nblocks*(nproc-1))).astype(int) # Divide the time axis into nblocks*(nproc -1) chunks.
+        equal_divide_size = np.round((len(times)-DMcurv_samples)/(nblocks*(nproc-1))).astype(int) # Divide the time axis into nblocks*(nproc -1) chunks.
     else:
-        equal_divide_size =  np.round(len(times)/nblocks).astype(int) # Divide the time axis into nblocks chunks.
+        equal_divide_size =  np.round((len(times)-DMcurv_samples)/nblocks).astype(int) # Divide the time axis into nblocks chunks.
     # Increase chunk size, if required, to make it exactly divisible by the temporal downsampling factor.
-    chunk_size = equal_divide_size + downsample_time_factor - (equal_divide_size % downsample_time_factor)
+    chunk_size = equal_divide_size + downsample_time_factor - (equal_divide_size % downsample_time_factor) # Chunks of the dedispersed time axis
     N_chunks = np.ceil(len(times)/chunk_size).astype(int) # No. of chunks required
     # Calculate time edges of each chunk.
     tstart = 0
-    tstop = chunk_size
+    tstop = chunk_size + DMcurv_samples
     tstart_values = [tstart] # List of start sample numbers (included) for each child processor
     tstop_values = [tstop] # List of stop sample numbers (excluded) for each child processor
     while tstop <= len(times):
-        tstart = tstop
-        tstop = tstop+chunk_size
+        tstart = tstop - DMcurv_samples
+        tstop = tstart + chunk_size + DMcurv_samples
         tstart_values.append(tstart)
         tstop_values.append(tstop)
-    # Drop samples from the final chunk to make its size divisible by the downsampling factor chosen.
-    tstop_values[-1] = tstart+int(np.floor((len(times) - tstart)/downsample_time_factor)*downsample_time_factor)
+    # Drop samples from the final chunk to make its size (after dedispersion) divisible by the downsampling factor chosen.
+    tstop_values[-1] = tstart + DMcurv_samples + int(np.floor((len(times) - DMcurv_samples - tstart)/downsample_time_factor)*downsample_time_factor)
     # Convert list to arrays.
     tstart_values = np.array(tstart_values, dtype=int)
     tstop_values = np.array(tstop_values, dtype=int)
@@ -159,7 +202,7 @@ def divide_timechunks(nproc, nblocks, times, nint, int_times, mask_zap_chans_per
     mask_zap_chans_per_int_values = [] # Set of channels to mask for every integration assigned to a child processor
     for i in range(len(tstart_values)):
         idx1 = np.where(int_times<=tstart_values[i]*t_samp)[0][-1]
-        idx2 = np.where(int_times<=tstop_values[i]*t_samp)[0][-1]
+        idx2 = np.where(int_times<tstop_values[i]*t_samp)[0][-1]
         nint_values.append(idx2-idx1+1)
         int_times_values.append(int_times[idx1:idx2+1])
         mask_zap_chans_per_int_values.append(mask_zap_chans_per_int[idx1:idx2+1])
@@ -184,8 +227,12 @@ def divide_timechunks(nproc, nblocks, times, nint, int_times, mask_zap_chans_per
 def set_defaults(hotpotato):
     if hotpotato['OUTPUT_DIR']=='':
         hotpotato['OUTPUT_DIR'] = hotpotato['DATA_DIR']
+    if hotpotato['write_format']=='':
+        hotpotato['write_format'] = 'npz'
     if hotpotato['N_blocks']=='':
         hotpotato['N_blocks'] = 1
+    if hotpotato['DM']=='':
+        hotpotato['DM'] = 0.
     if hotpotato['pol']=='':
         hotpotato['pol'] = 0
     if hotpotato['RFIMASK_DIR']=='':
@@ -218,6 +265,7 @@ def __MPI_MAIN__(parser):
 
         # Construct list of calls to run from shell.
         hotpotato = set_defaults(read_config(inputs_cfg))
+        hotpotato['basename'] = hotpotato['basename']+'_DM%.1f'% (hotpotato['DM'])
         parent_logger = setup_logger_stdout() # Set logger output of parent processor to stdout().
 
         parent_logger.info('Reading header of file: %s'% (hotpotato['fil_file']))
@@ -256,27 +304,32 @@ def __MPI_MAIN__(parser):
         # Load rfifind mask.
         parent_logger.info('Reading rfifind mask: %s'% (hotpotato['rfimask']))
         nint, int_times, ptsperint, mask_zap_chans, mask_zap_ints, mask_zap_chans_per_int = read_rfimask(hotpotato['RFIMASK_DIR']+'/'+hotpotato['rfimask'])
+        int_times = np.round(int_times/t_samp)*t_samp
         mask_zap_chans, mask_zap_chans_per_int = modify_zapchans_bandpass(mask_zap_chans, mask_zap_chans_per_int, hotpotato['ind_band_low'], hotpotato['ind_band_high'])
 
         # Section up time axis and rfifind mask into chunks.
-        tstart_values, tstop_values, nint_values, int_times_values, mask_zap_chans_per_int_values = divide_timechunks(nproc, hotpotato['N_blocks'], times, nint, int_times, mask_zap_chans_per_int, t_samp, hotpotato['kernel_size_time_samples'])
+        tstart_values, tstop_values, nint_values, int_times_values, mask_zap_chans_per_int_values = divide_timechunks(nproc, hotpotato['N_blocks'], times, nint, int_times, mask_zap_chans_per_int, t_samp, hotpotato['kernel_size_time_samples'], freqs_GHz[0], hotpotato['DM'], freqs_GHz[-1])
 
         create_dir(hotpotato['OUTPUT_DIR'])
-
         if nproc==1:
             for nb in range(len(tstart_values)):
                 times_chunk = np.arange(tstart_values[nb], tstop_values[nb])*t_samp
-                myexecute(hotpotato, times_chunk, tstart_values[nb], tstop_values[nb], freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_values[nb], int_times_values[nb], mask_zap_chans_per_int_values[nb], parent_logger, rank)
+                myexecute(hotpotato, times_chunk, tstart_values[nb], tstop_values[nb], freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_values[nb], int_times_values[nb], mask_zap_chans_per_int_values[nb], mask_zap_chans, parent_logger, rank)
         else:
             # Send data to child processors.
             for indx in range(1,nproc):
-                comm.send((hotpotato, t_samp, tstart_values[indx-1], tstop_values[indx-1], freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_values[indx-1], int_times_values[indx-1], mask_zap_chans_per_int_values[indx-1]), dest=indx, tag=indx)
+                comm.send((hotpotato, t_samp, tstart_values[indx-1], tstop_values[indx-1], freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_values[indx-1], int_times_values[indx-1], mask_zap_chans_per_int_values[indx-1], mask_zap_chans), dest=indx, tag=indx)
             comm.Barrier() # Wait for all child processors to receive sent call.
             # Receive Data from child processors after execution.
             comm.Barrier()
 
-        # Collate data from multiple .npz files output by child processors into a single filterbank file.
-        collate_npz(hdr, hotpotato, parent_logger, remove_npz=True)
+        '''
+        # Collate data from multiple temporary .npz files into a single .npz file or a filterbank file.
+        if hotpotato['write_format']=='npz':
+            collate_npz_to_npz(hdr, hotpotato, parent_logger, remove_npz=True)
+        elif  hotpotato['write_format']=='fil' or hotpotato['write_format']=='filterbank':
+            collate_npz_to_fil(hdr, hotpotato, parent_logger, remove_npz=True)
+        '''
 
         # Calculate total run time for the code.
         prog_end_time = time.time()
@@ -285,13 +338,13 @@ def __MPI_MAIN__(parser):
         print('FINISHING RANK 0')
     else:
         # Recieve data from parent processor.
-        hotpotato, t_samp, tstart_dist, tstop_dist, freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_dist, int_times_dist, mask_zap_chans_per_int_dist = comm.recv(source=0, tag=rank)
+        hotpotato, t_samp, tstart_dist, tstop_dist, freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_dist, int_times_dist, mask_zap_chans_per_int_dist, mask_zap_chans = comm.recv(source=0, tag=rank)
         comm.Barrier()
         print('STARTING RANK: ',rank)
         child_logger = setup_logger_stdout() # Set up separate logger for each child processor.
         for nb in range(len(tstart_dist)):
             times_chunk = np.arange(tstart_dist[nb], tstop_dist[nb])*t_samp
-            myexecute(hotpotato, times_chunk, tstart_dist[nb], tstop_dist[nb], freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_dist[nb], int_times_dist[nb], mask_zap_chans_per_int_dist[nb], child_logger, rank)
+            myexecute(hotpotato, times_chunk, tstart_dist[nb], tstop_dist[nb], freqs_GHz, npol, nchans, chan_bw, n_bytes, hdr_size, nint_dist[nb], int_times_dist[nb], mask_zap_chans_per_int_dist[nb], mask_zap_chans, child_logger, rank)
         print('FINISHING RANK: ',rank)
         comm.Barrier()
         # Send completed status back to parent processor.